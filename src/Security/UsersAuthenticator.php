566aa366a2f33dc1db3f2d72b4dc72f0f721302b
Cristiane <cristianemenna@gmail.com>
2020-01-23 23:49:38 +0100

    Configuration de redirection à l'index après login

--- src/Security/UsersAuthenticator.php
+++ src/Security/UsersAuthenticator.php
@@ -95,7 +95,8 @@ class UsersAuthenticator extends AbstractFormLoginAuthenticator implements Passw
         }
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('users_index'));
     }
 
     protected function getLoginUrl()
