5fa14271db033191de60b344f30beccd5d6a506d
Cristiane <cristianemenna@gmail.com>
2020-02-19 18:06:18 +0100

    Création d'entité Import et ManyToOne avec Context

---
+++ src/Entity/Import.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ImportRepository")
+ */
+class Import
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $file;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Context", inversedBy="imports")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $context;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Log", mappedBy="import", orphanRemoval=true)
+     */
+    private $logs;
+
+    public function __construct()
+    {
+        $this->logs = new ArrayCollection();
+        $this->status = "En attente";
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFile(): ?string
+    {
+        return $this->file;
+    }
+
+    public function setFile(string $file): self
+    {
+        $this->file = $file;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getContext(): ?Context
+    {
+        return $this->context;
+    }
+
+    public function setContext(?Context $context): self
+    {
+        $this->context = $context;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Log[]
+     */
+    public function getLogs(): Collection
+    {
+        return $this->logs;
+    }
+
+    public function addLog(Log $log): self
+    {
+        if (!$this->logs->contains($log)) {
+            $this->logs[] = $log;
+            $log->setImport($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLog(Log $log): self
+    {
+        if ($this->logs->contains($log)) {
+            $this->logs->removeElement($log);
+            // set the owning side to null (unless already changed)
+            if ($log->getImport() === $this) {
+                $log->setImport(null);
+            }
+        }
+
+        return $this;
+    }
+}
