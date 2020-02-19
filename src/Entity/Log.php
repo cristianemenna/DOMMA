1e377d962a5d2c3f299575fec5faf93da3f9b240
Cristiane <cristianemenna@gmail.com>
2020-02-19 18:07:09 +0100

    Création d'entité Log et ManyToOne avec Import

---
+++ src/Entity/Log.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\LogRepository")
+ */
+class Log
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $created_at;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $message;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Import", inversedBy="logs")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $import;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+
+    public function getImport(): ?Import
+    {
+        return $this->import;
+    }
+
+    public function setImport(?Import $import): self
+    {
+        $this->import = $import;
+
+        return $this;
+    }
+}
