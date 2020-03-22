b4b52b383a4a56ee4c14ec33ac6d58dcdbd1f555
Cristiane <cristianemenna@gmail.com>
2020-03-22 17:36:22 +0100

    Select2 sur la page de création de macro

--- public/javascript/select_apply.js
+++ public/javascript/select_apply.js
@@ -1,8 +1,13 @@
-console.log('Ćoucou');
-
 $(document).ready(function() {
     $('#users_roles').select2({
         allowClear: true,
         placeholder: "Rôle"
+    });
+});
+
+$(document).ready(function() {
+    $('#macro_type').select2({
+        placeholder: "Type",
+        allowClear: true
     });
 });
