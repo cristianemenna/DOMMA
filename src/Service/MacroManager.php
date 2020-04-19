420b5590bcf8ed956dd1fba8076352520201bd90
Cristiane <cristianemenna@gmail.com>
2020-04-19 17:17:19 +0200

    Correction de la méthode 'select', pour ajouter l'id de chaque ligne sur update

--- src/Service/MacroManager.php
+++ src/Service/MacroManager.php
@@ -42,9 +42,6 @@ class MacroManager
             case 'delete':
                 $this->delete($macro, $import);
                 break;
-            case 'tiret-par-espace':
-                $this->replaceHyphenBySpace($macro, $import);
-                break;
         }
     }
 
@@ -68,7 +65,7 @@ class MacroManager
         $tableName = $dataBase->quoteIdentifier('import_'. strval($import->getId()));
 
         // Création de la requête avec le code de la macro
-        $requestSQL = 'SELECT ' . $macroCode .
+        $requestSQL = 'SELECT id AS query_id, ' . $macroCode .
                         ' FROM ' . $schemaName . '.' . $tableName;
         $statement = $dataBase->executeQuery($requestSQL);
 
@@ -97,10 +94,13 @@ class MacroManager
 
         $columnsKeys = array_keys($columns);
         foreach ($columns as $columnName => $value) {
-            if ($columnName !== end($columnsKeys)) {
-                $requestSQL .= ' ADD COLUMN IF NOT EXISTS ' . $columnName . ' TEXT,';
-            } else {
-                $requestSQL .= ' ADD COLUMN IF NOT EXISTS ' . $columnName . ' TEXT';
+            // N'ajoute pas la colonne qui contient les id
+            if ($columnName !== 'query_id') {
+                if ($columnName !== end($columnsKeys)) {
+                    $requestSQL .= ' ADD COLUMN IF NOT EXISTS ' . $columnName . ' TEXT,';
+                } else {
+                    $requestSQL .= ' ADD COLUMN IF NOT EXISTS ' . $columnName . ' TEXT';
+                }
             }
         }
 
@@ -126,31 +126,28 @@ class MacroManager
 
         // Boucle sur chaque ligne du résultat de la requête
         foreach ($content as $key => $line) {
-            $requestSQL = 'UPDATE ' . $schemaName . '.' . $tableName . ' SET (';
+            $requestSQL = 'UPDATE ' . $schemaName . '.' . $tableName . ' SET ';
 
             // Ajoute les noms des colonnes à modifier sur chaque ligne
+            $id = null;
             $lineKeys = array_keys($line);
             foreach ($line as $column => $contentValue) {
-                if ($column !== end($lineKeys)) {
-                    $requestSQL .= $column . ', ';
+                // On ne lit pas les données de la première colonne (id)
+                if ($column !== 'query_id') {
+                    // Actualise chaque colonne avec la valeur correspondante dans le résultat du select
+                    if ($column !== end($lineKeys)) {
+                        $requestSQL .= $column . ' ' . '=' . ' ' . "'" . $contentValue . "', ";
+                    } else {
+                        $requestSQL .= $column . ' ' . '=' . ' ' . "'" . $contentValue . "'";
+                    }
+                // Recupère l'id de chaque ligne pour l'utiliser ensuite
                 } else {
-                    $requestSQL .= $column;
-                }
-            }
-
-            $requestSQL .= ') = (';
-
-            // Ajoute les valeurs à ajouter sur chaque colonne
-            foreach ($line as $column => $contentValue) {
-                if ($column !== end($lineKeys)) {
-                    $requestSQL .= $contentValue . ', ';
-                } else {
-                    $requestSQL .= $contentValue;
+                    $id = $contentValue;
                 }
             }
 
             // Indique l'id de chaque ligne depuis la première
-            $requestSQL .= ') WHERE id = ' . ($key + 1);
+            $requestSQL .= ' WHERE id = ' . $id;
             $dataBase->executeQuery($requestSQL);
         }
     }
@@ -209,29 +206,6 @@ class MacroManager
         $requestSQL = 'DELETE FROM ' . $schemaName . '.' . $tableName .
                         ' WHERE ' . $macroCode;
 
-        $dataBase->executeQuery($requestSQL);
-    }
-
-    /** Application de macro qui substitue les tirets par espaces vide dans une colonne
-     *
-     * @param MacroApplyManager $macro
-     * @param Import $import
-     * @throws \Doctrine\DBAL\DBALException
-     */
-    private function replaceHyphenBySpace(MacroApplyManager $macro, Import $import)
-    {
-        $dataBase = $this->entityManager->getConnection();
-        // Recupère le code de la macro
-        $macroCode = $macro->getMacro()->getCode();
-
-        // Recupère le nom du contexte pour identifier le nom du schema de l'import
-        $schemaName = $dataBase->quoteIdentifier(mb_strtolower($import->getContext()->getTitle()));
-        $schemaName = str_replace([' '], '_', $schemaName);
-        // Recupère le nom de la table de l'import
-        $tableName = $dataBase->quoteIdentifier('import_'. strval($import->getId()));
-
-        // Création de la requête avec le code de la macro
-        $requestSQL = 'UPDATE ' . $schemaName . '.' . $tableName . ' SET ' . $macroCode . ' = REPLACE(' . $macroCode . ", '-', ' ')";
         $dataBase->executeQuery($requestSQL);
     }
 
