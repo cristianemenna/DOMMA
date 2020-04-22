787be5df4dc347297d49d68af36802d95163f8a5
Cristiane <cristianemenna@gmail.com>
2020-04-22 15:49:59 +0200

    Ajout d'attribut fileName sur Import et migration correspondante

---
+++ src/Migrations/Version20200422132833.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20200422132833 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('ALTER TABLE import ADD file_name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE import RENAME COLUMN file TO file_path');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SCHEMA public');
+        $this->addSql('ALTER TABLE import ADD file VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE import DROP file_path');
+        $this->addSql('ALTER TABLE import DROP file_name');
+    }
+}
