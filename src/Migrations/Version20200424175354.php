095a52938757543ec6c6e5af7f20b22b2b17a816
Cristiane <cristianemenna@gmail.com>
2020-04-28 11:20:50 +0200

    Migration avec ON DELETE CASCADE sur Import + création de fonction pour suppression des contextes expirés

--- src/Migrations/Version20200424175354.php
+++ src/Migrations/Version20200424175354.php
@@ -24,6 +24,23 @@ final class Version20200424175354 extends AbstractMigration
 
         $this->addSql('ALTER TABLE import DROP CONSTRAINT FK_9D4ECE1D6B00C1CF');
         $this->addSql('ALTER TABLE import ADD CONSTRAINT FK_9D4ECE1D6B00C1CF FOREIGN KEY (context_id) REFERENCES context (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('
+                            CREATE OR REPLACE FUNCTION remove_expired_contexts() RETURNS text AS $$
+                            DECLARE query RECORD;
+                            DECLARE iterator INTEGER := 0;
+                            BEGIN
+                                FOR query IN SELECT id, lower(title) as title
+                                FROM context
+                                WHERE created_at + make_interval(days => duration) <= CURRENT_TIMESTAMP
+                              LOOP
+                                EXECUTE format(\'DROP SCHEMA %I CASCADE\', query.title);
+                                DELETE FROM context WHERE id = query.id;
+                                    iterator := iterator + 1;
+                               END LOOP;
+                            RETURN iterator || \' contextes supprimés\';
+                            END;
+                            $$ LANGUAGE plpgsql;
+        ');
     }
 
     public function down(Schema $schema) : void
@@ -34,5 +51,6 @@ final class Version20200424175354 extends AbstractMigration
         $this->addSql('CREATE SCHEMA public');
         $this->addSql('ALTER TABLE import DROP CONSTRAINT fk_9d4ece1d6b00c1cf');
         $this->addSql('ALTER TABLE import ADD CONSTRAINT fk_9d4ece1d6b00c1cf FOREIGN KEY (context_id) REFERENCES context (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('DROP FUNCTION remove_expired_contexts()');
     }
 }
