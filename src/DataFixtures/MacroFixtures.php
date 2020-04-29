267cc76e93023ac1b7aa0b09ce3d726f458d2f26
Cristiane <cristianemenna@gmail.com>
2020-04-29 20:18:00 +0200

    Adaptation des fixtures de Macros au refactor

--- src/DataFixtures/MacroFixtures.php
+++ src/DataFixtures/MacroFixtures.php
@@ -15,14 +15,14 @@ class MacroFixtures extends Fixture implements OrderedFixtureInterface
         $macroOne = new Macro();
         $macroOne->setTitle('Filtre sur colonne IDENTIFIANT GLOBAL');
         $macroOne->setDescription('Affiche seulement les lignes du fichiers qui contient un \'Identifant Global Groupe commençant par \'5\'');
-        $macroOne->setCode('"identifiant global groupe (igg)" NOT LIKE \'5%\'');
+        $macroOne->setCode('"Identifiant Global Groupe (IGG)" NOT LIKE \'5%\'');
         $macroOne->setType('delete');
 
         // Exemple de recherche de doublon
         $macroTwo = new Macro();
         $macroTwo->setTitle('Recherche des doublons sur CODE ONE et LANGUE');
         $macroTwo->setDescription('Afficher le nombre d\'ocurrences identiques sur les deux colonnes');
-        $macroTwo->setCode('count("code one") over (partition by "code one") as nb_dubble_code_one, count("langue") over (partition by "langue") as nb_dubble_langue');
+        $macroTwo->setCode('count("Code ONE") over (partition by "Code ONE") as nb_dubble_code_one, count("Langue") over (partition by "Langue") as nb_dubble_langue');
         $macroTwo->setType('select');
 
         // Exemple de filtre sur lignes avec doublons
@@ -36,21 +36,21 @@ class MacroFixtures extends Fixture implements OrderedFixtureInterface
         $macroFour = new Macro();
         $macroFour->setTitle('Création de clé d\'authentication unique');
         $macroFour->setDescription('Création de clé unique à partir de la concaténation de champs : 3 premiers caractères de l\entité + 4 premiers caractères du igg');
-        $macroFour->setCode('SUBSTR("entité actuelle", 1, 3) || SUBSTR("identifiant global groupe (igg)", 1, 4) as cle_authentication');
+        $macroFour->setCode('SUBSTR("Entité actuelle", 1, 3) || SUBSTR("Identifiant Global Groupe (IGG)", 1, 4) as cle_authentication');
         $macroFour->setType('select');
 
         // Exemple d'affichage des champs selon critère
         $macroFive = new Macro();
         $macroFive->setTitle('Affiche seulement les 3 premiers caractères du IGG');
         $macroFive->setDescription('Si le IGG est supérieur à 3 caractères, affiche seulement les trois premiers');
-        $macroFive->setCode('"identifiant global groupe (igg)" = SUBSTR("identifiant global groupe (igg)", 1, 3) WHERE LENGTH("identifiant global groupe (igg)") > 3');
+        $macroFive->setCode('"Identifiant Global Groupe (IGG)" = SUBSTR("Identifiant Global Groupe (IGG)", 1, 3) WHERE LENGTH("Identifiant Global Groupe (IGG)") > 3');
         $macroFive->setType('update');
 
         // Exemple de remplacement de caractères
         $macroSix = new Macro();
         $macroSix->setTitle('Remplace tiret par espace vide sur LANGUE');
         $macroSix->setDescription('Remplace tous les tirets par des espaces vides sur la colonne langue');
-        $macroSix->setCode("langue = REPLACE(langue, '-', ' ')");
+        $macroSix->setCode('"Langue" = REPLACE("Langue", "-", " ")');
         $macroSix->setType('update');
 
         $user = $this->getReference('user');
