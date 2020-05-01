3915ac9c5a385a104953b47140adb1ea2ed3d9c8
Cristiane <cristianemenna@gmail.com>
2020-05-01 13:46:02 +0200

    Ajout de validation sur le format des mails

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -6,6 +6,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UsersRepository")
@@ -37,6 +38,7 @@ class Users implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Email
      */
     private $email;
 
