7646d2fa71ab162949e21808451d14719c7a23d7
Cristiane <cristianemenna@gmail.com>
2020-05-04 23:33:47 +0200

    Ajout de formulaire de suppression de colonnes sur show Import

--- src/Controller/ImportController.php
+++ src/Controller/ImportController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Import;
 use App\Form\MacroApplyType;
+use App\Form\MacroColumnsType;
 use App\Repository\ImportRepository;
 use App\Service\GravatarManager;
 use App\Service\LoadFileManager;
@@ -28,12 +29,17 @@ class ImportController extends AbstractController
     /**
      * @Route("/{id}", name="import_show", methods={"GET", "POST"})
      */
-    public function show(Request $request, Import $import, GravatarManager $gravatar, LoadFileManager $loadFileManager, MacroManager $macroManager, SessionInterface $session): Response
+    public function show(Request $request,
+                         Import $import,
+                         GravatarManager $gravatar,
+                         LoadFileManager $loadFileManager,
+                         MacroManager $macroManager,
+                         ImportRepository $importRepository,
+                         SessionInterface $session): Response
     {
         $user = $this->getUser();
         // Si l'utilisateur actif n'as pas droit d'accès au contexte auquel appartient l'import, on affiche un 'Not found'
-        if (!$import->getContext()->getUsers()->contains($user))
-        {
+        if (!$import->getContext()->getUsers()->contains($user)) {
             throw $this->createNotFoundException();
         }
 
@@ -53,11 +59,17 @@ class ImportController extends AbstractController
         }
 
         $macro = new MacroApplyManager();
-        $form = $this->createForm(MacroApplyType::class, $macro, ['macros' => $user->getMacros()]);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
+        $columns = new MacroApplyManager();
+
+        $macrosForm = $this->createForm(MacroApplyType::class, $macro, ['macros' => $user->getMacros()]);
+        $macrosForm->handleRequest($request);
+
+        // Traitements lors de l'envoie des formulaires des Macros
+        if ($macrosForm->isSubmitted() && $macrosForm->isValid()) {
+            // Si le choix est 'Voir macro en détail', redirige à la route d'édition de macro
             if (isset($_POST['details'])) {
                 return $this->redirectToRoute('macro_edit', ['id' => $macro->getMacro()->getId()]);
+            // Sinon, exécute le code d'application de macro
             } else {
                 // Exécute la requête en BDD de la macro séléctionnée
                 try {
@@ -71,13 +83,29 @@ class ImportController extends AbstractController
             }
         }
 
+        $columnsForm = $this->createForm(MacroColumnsType::class, $columns, ['columns' => $importColumns]);
+        $columnsForm->handleRequest($request);
+
+        // Traitement lors de l'envoie du formulaire de suppression de colonnes
+        if ($columnsForm->isSubmitted() && $columnsForm->isValid()) {
+            try {
+                $importRepository->removeColumns($import, $columns);
+                $importContent = $loadFileManager->showTable($import, 'content');
+                $importColumns = $loadFileManager->showTable($import, 'columns');
+                $this->addFlash('success', 'Les colonnes ont bien été supprimées.');
+            } catch (\Exception $e) {
+                $this->addFlash('error', $e->getMessage());
+            }
+        }
+
         return $this->render('import/show.html.twig', [
             'avatar' => $gravatar->getAvatar($user),
             'import' => $import,
             'importContent' => $importContent,
             'importColumns' => $importColumns,
             'macros' => $macros,
-            'macroForm' => $form->createView(),
+            'macroForm' => $macrosForm->createView(),
+            'columnsForm' => $columnsForm->createView(),
         ]);
     }
 
