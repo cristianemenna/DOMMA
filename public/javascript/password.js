69d2ab9cb58976e53e91d495346616bd73b01765
Cristiane <cristianemenna@gmail.com>
2020-05-16 18:37:23 +0200

    Ajoute element sur le DOM au lieu de alert() pour affichage de message

--- public/javascript/password.js
+++ public/javascript/password.js
@@ -1,19 +1,33 @@
-const submitButton = document.getElementById('submit-button');
+var submitButton = document.getElementById('submit-button');
 submitButton.addEventListener('click', validatePassword);
 
 // Valide si les deux champs du nouveau de passe correspondent
 function validatePassword() {
-    const form = document.getElementsByTagName('form')[0];
+    var form = document.getElementsByTagName('form')[0];
     form.addEventListener('submit', function (event) {
         event.preventDefault();
     });
 
-    const firstField = document.getElementById('users_password_password').value;
-    const secondField = document.getElementById('password-confirmation').value;
+    var formErrors = document.getElementsByClassName('form-errors-container')[0];
+    // Supprime le message d'erreur précedente (s'il y en a)
+    if (formErrors.childNodes.length > 0) {
+        while (formErrors.lastChild) {
+            formErrors.removeChild(formErrors.lastChild);
+        }
+    }
+
+    var firstField = document.getElementById('users_password_password').value;
+    var secondField = document.getElementById('password-confirmation').value;
 
     if (firstField !== secondField) {
-        alert('Les deux champs ne correspondent pas.');
+        // Affiche un message d'erreur si l'email n'est pas valide
+        var ul = document.createElement('ul');
+        var li = document.createElement('li');
+        li.innerText = 'Les deux champs ne correspondent pas.';
+        formErrors.appendChild(ul);
+        ul.appendChild(li);
     } else {
         form.submit();
     }
 }
+
